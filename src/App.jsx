f7122748a6884caefc1d61dbfd8e29321b2811f3
gitkraken303 <johnspaxxvile@gmail.com>
2023-07-26 15:47:43 +0300

    initial folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl text-red-600">Hello world</h2>
+    </>
+  );
+}
+
+export default App;
